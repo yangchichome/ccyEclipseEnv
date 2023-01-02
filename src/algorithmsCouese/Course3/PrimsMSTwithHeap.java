746352f6350724ede765d23c991de0ba9d5d3c6b
yangchichome <118092513+yangchichome@users.noreply.github.com>
2023-01-02 10:17:43 +0800

    added PrimsMST

---
+++ src/algorithmsCouese/Course3/PrimsMSTwithHeap.java
@@ -0,0 +1,15 @@
+package algorithmsCouese.Course3;
+
+import java.util.PriorityQueue;
+
+public class PrimsMSTwithHeap {
+
+	private static PriorityQueue<Integer> V_X = new PriorityQueue<>();
+//	private static PriorityQueue<Integer> V_X = new PriorityQueue<>();6
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
