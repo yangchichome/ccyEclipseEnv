b356a5255180387cbc1491311d09e362c3827bf3
yangchichome <118092513+yangchichome@users.noreply.github.com>
2023-01-26 14:12:10 +0800

    TSP Implement cluster

--- src/algorithmsCouese/Course4/tsp.java
+++ src/algorithmsCouese/Course4/tsp.java
@@ -65,14 +65,14 @@ public class tsp {
 		initialCluster();
 		
 		//do cluster
-		while(clusters.size()>22) {
+		while(clusters.size()>21) {
 			edgeCost minCost = edgeCosts.poll();
 			mergeCluster(minCost);
 		}
 		
 		//after cluster 
 		int N = clusters.size();
-		A = new Double[1 << N][N];
+		A = new Double[1 << N][N+1];
 		Arrays.stream(A).forEach(a -> Arrays.fill(a, Double.MAX_VALUE));
 		A[1][1]=0.0;
 		//set map for bitMask
@@ -83,90 +83,110 @@ public class tsp {
 		}
 		
 		//for loop for subproblem
-		int minPath = Integer.MAX_VALUE;
 		for (int m=2;m<=N;m++) {
 			//get S include 1,with size m
 			HashSet<Integer> bitMasks = new HashSet<Integer>() ;
 			bitMasks = getSet(m,N);
-			System.out.println("get SebSet of M"+m);
-			System.out.println("subSet size "+bitMasks.size());
+//			System.out.println("get SebSet of M"+m);
+//			System.out.println("subSet size "+bitMasks.size());
 //			for (int sub : bitMasks) {
 //				System.out.println(" "+Integer.toBinaryString(sub));
 //			}
 			//for each set 1...n , set.contain(j) ,j != 1
 			for (int S : bitMasks) {
-				String setStr = Integer.toBinaryString(S);
-				System.out.println("sub: "+setStr);
-		        for (int i = 1; i < setStr.length(); i++) {
+				String Sstr = Integer.toBinaryString(S);
+//				System.out.println("sub: "+Sstr);
+		        for (int i = 1; i < Sstr.length(); i++) {
 		        	char str1 = '1';
-		        	if (setStr.charAt(i) == str1) {
-		        		System.out.println(" "+i);
-		        		int j = i;
-//		        		A[S,j] = min k,j A[S-{j},k]+ckj];
-		        		minPath = setA2D(S,j);
+		        	if (Sstr.charAt(i) == str1) {
+		        		int j = i+1;
+		        		A[S][j] = setA2D(S,j);
+//		        		System.out.println("J: "+j+"value: "+A[S][j]);
 		        	}
 		        }
 			}
-			System.out.println("SebSet End");
-//			bitMasks.clear();
+//			System.out.println("SebSet End");
 		}
-		
+		//Return min
+		ArrayList<Integer> tour = new ArrayList<Integer>();
+		int FullS = 1;
+		for (int i=1;i<N;i++) {
+			FullS = FullS | 1<<i;
+		}
+		tour.add(1);
+		int tmpJ = 0;
+		int tmpk = 1;
+		int tmpS = FullS;
+		while (tour.size()<N) {
+			double minPath = Double.MAX_VALUE;
+			int nodeT = bitMaps.get(tmpk);
+			System.out.print("tmp J: "+nodeT+" child:");
+			for (int child:clusters.get(nodeT)) {
+				System.out.print(" "+child);
+			}
+			System.out.println("");
+			for(int j=2;j<=N;j++) {
+				if (!tour.contains(j)) {
+					double value = A[tmpS][j]+getPath(j,tmpk);
+//					System.out.println("J: "+j+" value "+value);
+					if (minPath > value) {
+						tmpJ = j;
+						minPath = value;
+					}
+				}
+			}
+
+//			System.out.println("tmpS:"+Integer.toBinaryString(tmpS));
+//			System.out.println("tmpS:"+Integer.toBinaryString(1 << tmpJ-1));
+			tmpS = FullS - (1 << tmpJ-1);
+//			System.out.println("tmpS:"+Integer.toBinaryString(tmpS));
+			tmpk = tmpJ;
+			
+			tour.add(tmpJ);
+		}
+//		int Jreal = bitMaps.get(finalJ);
+//		System.out.println("Final J: "+Jreal);
+//		System.out.println("minPath: "+minPath);
 		System.out.println("End");
 	}
 
-	private static int setA2D(int S, int j) {
+	private static Double setA2D(int S, int j) {
 		// TODO Auto-generated method stub
-		String setStr = Integer.toBinaryString(S);
-//		int jNew = bitMaps.get(j);
-		int J = 1 << j;
-		int subS_j = S - (1 << j);
-		String setStr2 = Integer.toBinaryString(J);
-		String setStr3 = Integer.toBinaryString(subS_j);
-		System.out.println(setStr);
-		System.out.println(setStr2);
-		System.out.println(setStr3);
-		
-        for (int i = 0; i < setStr.length(); i++) {
+		int S_j = S - (1 << j-1);
+		String Sstr = Integer.toBinaryString(S);
+//		String SJstr = Integer.toBinaryString(S_j);
+//		System.out.println(Sstr);
+//		System.out.println(SJstr);
+		Double minValue = Double.MAX_VALUE;
+        for (int i = 0; i < Sstr.length(); i++) {
         	char str1 = '1';
-        	if (setStr.charAt(i) == str1) {
-//        		System.out.print(" "+i);
-        		
+        	if (Sstr.charAt(i) == str1) {
         		int k = i+1;
         		if (k!=j) {
-        			
-//        			int minPath = setA2D(A,S,j);
+        			if (A[S_j][k] == Double.MAX_VALUE) continue;
+        			Double value = A[S_j][k]+getPath(j,k);
+        			if (minValue > value) {
+        				minValue = value;
+        			}
         		}
         	}
         }
-		return 0;
+		return minValue;
 	}
 
-//	private static void setbitMaskMap() {
-//		// TODO Auto-generated method stub
-//		int index =0;
-//		//get SubSet
-//		System.out.println("get subset");
-//		for (int m=2;m<N;m++) {
-//			int b = 1;
-//			int name = 0;
-//			generatebitMasks(0,b,m,name);
-//			
-//			System.out.println("m = "+m);
-//			System.out.println("final Size "+bitMasks.size());
-//		}
-//		System.out.println("set subset to Hash");
-//		for (int S : bitMasks) {
-////			String setStr = Integer.toBinaryString(S);
-////			System.out.println("sub: "+setStr);
-//			if (index <= 9000000) {
-//				bitMask9M1.put(S, index++);
-//			}else {
-//				bitMask9M2.put(S, index++);
-//			}
-////			bitMask9M1.put(S, index++);
-////			System.out.println(" index "+index);
-//		}
-//	}
+	private static Double getPath(int j, int k) {
+		// TODO Auto-generated method stub
+		int jNew = bitMaps.get(j);
+		int kNew = bitMaps.get(k);
+		nodeXY nj = nodes.get(jNew-1);
+		nodeXY nk = nodes.get(kNew-1);
+		
+		double dis_X = (nj.x - nk.x)*(nj.x - nk.x);
+		double dis_Y = (nj.y - nk.y)*(nj.y - nk.y);
+		double cost = Math.sqrt(dis_X+dis_Y);
+		
+		return cost;
+	}
 
 	private static HashSet<Integer> getSet(int m, int n) {
 		// TODO Auto-generated method stub
@@ -197,68 +217,6 @@ public class tsp {
 		}
 		return bitMasks;
 	}
-//	private static Object[] setDisMap() throws IOException {
-//		// TODO Auto-generated method stub
-//		BufferedReader input = new BufferedReader(new FileReader(file));
-//		Double[][] distance;
-//		ArrayList<Integer> coorSet = new ArrayList<>(); 
-//		
-//		try {
-//			// Read a line of text from the file
-//			String line = input.readLine();
-//			int size = Integer.valueOf(line);
-//			distance = new Double[size][size];
-//			Double[][] coor = new Double[size][2];
-//			line = input.readLine();
-//			int i =0;
-//			while (line != null) {
-//				// Split the line of text Longo a vector of Strings
-//				String[] coordinate = line.split(" ");
-//				
-//				coor[i][0]= Double.valueOf(coordinate[0]);
-//				coor[i][1]= Double.valueOf(coordinate[1]);
-//				// Read a new line of text
-//				coorSet.add(i);
-//				i++;
-//				line = input.readLine();
-//			} // while
-//			for (int s=0;s<distance.length;s++) {
-//				for(int e=0;e<distance[0].length;e++) {
-//					double disX = coor[s][0]-coor[e][0];
-//					double disY = coor[s][1]-coor[e][1];
-//					double disFinal = Math.sqrt(disX*disX+disY*disY);
-//					distance[s][e]=disFinal;
-//				}
-//			}
-//			
-//		} // try
-//		finally {
-//			input.close();
-//		} // finally
-//		Object[] dis = new Object[2];
-//		dis[0] = distance;
-//		dis[1] = coorSet;
-//		return dis;
-//	}
-//	private static int getSize() throws IOException {
-//	// TODO Auto-generated method stub
-//		BufferedReader input = new BufferedReader(new FileReader(file));
-//		int size = 0;
-//		try {
-//			// Read a line of text from the file
-//			String line = input.readLine();
-//			size = Integer.valueOf(line);
-//			
-//			while (line != null) {
-//				// Split the line of text Longo a vector of Strings
-//				line = input.readLine();
-//			} // while
-//		} // try
-//		finally {
-//			input.close();
-//		} // finally
-//		return size;
-//	}
 	private static void mergeCluster(edgeCost minCost) {
 		// merge and clean cluster
 		System.out.println("merge node1: "+minCost.node1+" and node2: "+minCost.node2+" Cost : "+minCost.cost);
@@ -272,12 +230,18 @@ public class tsp {
 		Set<Integer> nodes2 = new HashSet<>();
 		nodes2 = clusters.get(node2);
 		nodes1.addAll(nodes2);
-		
-		clusters.remove(node2);
-		
-		clusters.put(node1, nodes1);
-		for(int node : nodes1) {
-			nodeParent.put(node,node1);
+		if (node1<node2) {
+			clusters.remove(node2);
+			clusters.put(node1, nodes1);
+			for(int node : nodes1) {
+				nodeParent.put(node,node1);
+			}
+		}else {
+			clusters.remove(node1);
+			clusters.put(node2, nodes1);
+			for(int node : nodes1) {
+				nodeParent.put(node,node2);
+			}
 		}
 	}
 	
